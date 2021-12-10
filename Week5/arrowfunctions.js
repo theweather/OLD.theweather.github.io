045fa3ddfb6be3c1e49eb90cf1be0cdc68b260a2
Shawna <skjacques@gmail.com>
2021-12-10 00:23:55 -0800

    Initial week 5 commit.

---
+++ Week5/arrowfunctions.js
@@ -0,0 +1,19 @@
+function addFive(num) {
+  return num + 5;
+}
+// TODO: Convert addFive() to arrow function
+let addFive = num => num + 5;
+
+
+function divideBy(x, y) {
+  return x / y;
+}
+// TODO: Convert divideBy() to arrow function
+let divideBy = (x, y) => x/y;
+
+function logToConsole() {
+  let msg = "MIT Coding Certificate";
+  console.log(msg);
+}
+// TODO: Convert logToConsole() to arrow function
+let logToConsole = msg => cnosole.log("MIT Coding Certificate");
